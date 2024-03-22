633c103dd855eff0e9c62589e73aa74ffc07f566
Pepa Bůh <128801641+PalacinkaVelika@users.noreply.github.com>
2024-03-22 18:37:48 +0100

    api1

--- Cviceni3/app.py
+++ Cviceni3/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request, session, flash, redirect, url_for, get_flashed_messages, jsonify
-
+from flask import Flask, render_template, request, session, flash, redirect, url_for, get_flashed_messages, jsonify, make_response
+import json
 
 
 app = Flask(__name__, template_folder='templates')
@@ -51,9 +51,27 @@ def objednej():
         return redirect("/")
     return render_template("objednej.html")
 
+@app.route("/api/picy", methods=["GET"])
+def api_get_picy():
+    return make_response(json.dumps(nabidka), 200)
 
+@app.route("/api/pica", methods=["GET"])
+def api_get_pica():
+    nazev = request.args.get("jmeno")
+    # Check if the name parameter is provided
+    if not nazev:
+        return jsonify({"error": "Parameter 'jmeno' is missing"}), 400
+    # Search for the pizza in the menu
+    selected_pizza = None
+    for pizza in nabidka:
+        if pizza["název"] == nazev:
+            selected_pizza = pizza
+            break
+    if selected_pizza:
+        return jsonify(selected_pizza), 200
+    else:   
+        return jsonify({"error": f"Pizza '{nazev}' not found"}), 404
 
 if __name__ == "__main__":
-    
     app.run(host="0.0.0.0", port=5000, debug=True)
     
